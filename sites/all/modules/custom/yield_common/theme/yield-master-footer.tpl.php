538267ba930c2e2fdeff7402a0d5938de4b81d4b
sanya-popruzhuk <sanya.popruzhuk@gmail.com>
2015-02-21 12:15:54 +0200

    21.02: front page panes, footer pane

--- sites/all/modules/custom/yield_common/theme/yield-master-footer.tpl.php
+++ sites/all/modules/custom/yield_common/theme/yield-master-footer.tpl.php
@@ -6,9 +6,11 @@
 */
 ?>
 <footer class="master_footer">
-    <div class="section_main_footer">
-        &copy; YIELD 2014 - <?php print date('Y'); ?>
-        All rights reserved
+    <div class="footer_left">
+      &copy; YIELD 2014 - <?php print date('Y'); ?>
+    </div>
+    <div class="footer_right">     
+      All rights reserved
     </div>
 </footer>
 
