fa9626090384eab38d8c8a2f922bb44ff92e7468
sanya-popruzhuk <sanya.popruzhuk@gmail.com>
2015-02-21 10:55:58 +0200

    21.02: Created panels and pane for default and front pages

---
+++ sites/all/themes/sgblank/js/scripts.js
@@ -0,0 +1,9 @@
+(function ($, Drupal) {
+
+  Drupal.behaviors.sgblank = {
+    attach: function(context, settings) {
+      
+    }
+  };
+
+})(jQuery, Drupal);
--- sites/all/themes/yield/template.php
+++ sites/all/themes/yield/template.php
@@ -4,3 +4,11 @@
  * @file
  * template.php
  */
+
+function yield_preprocess_page(&$variables) {
+// Проверим, является ли эта страницей - главной страницей
+if(drupal_is_front_page()) {
+    // Уберём default_message с главной
+    unset($variables['page']['content']['system_main']['default_message']);
+  }
+}
