a6b6765d489b29786f093df5fde76b344fb31834
sanya-popruzhuk <sanya.popruzhuk@gmail.com>
2015-03-12 17:56:39 +0200

    12.03: map slide effect

--- sites/all/themes/yield_zen/js/script.js
+++ sites/all/themes/yield_zen/js/script.js
@@ -30,10 +30,11 @@ Drupal.behaviors.my_custom_behavior = {
     });
 
     $(document).ready(function() {
-
+      $('.tooltip-show').add();
       $('.tooltip-show').hide();
       $( ".land" ).mouseover(function(event) {
         $(this).mousemove(function( event ) {
+          $('.tooltip-show').add();
           $('.tooltip-show').css({'top':event.pageY-50,'left':event.pageX-50, 'position':'absolute', 'border':'1px solid black', 'padding':'5px'});
           $('.tooltip-show').show();
 
@@ -45,8 +46,19 @@ Drupal.behaviors.my_custom_behavior = {
             $('.tooltip-show').empty();
       });
       $('.land').click(function() {
-        alert($(this).attr("id"));
+        $('.tooltip-show').hide();
+        var container = document.getElementById( 'st-container' );
+        $(container).addClass('st-effect-2');
+        setTimeout( function() {
+          $(container).addClass('st-menu-open');
+        }, 25 );
       });
+      $('#st-container').click(function() {
+        if ($(this).hasClass('st-effect-2 st-menu-open')) {
+            $(this).removeClass('st-effect-2 st-menu-open');
+          }
+      });
+      
 
       $('#graph').click(function(e) {
         return false;
