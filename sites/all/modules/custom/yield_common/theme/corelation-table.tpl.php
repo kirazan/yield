8e16b75f276d51730ab22f1241adea19c872d1b1
sanya-popruzhuk <sanya.popruzhuk@gmail.com>
2015-06-10 18:56:40 +0300

    10.06: Correlation

---
+++ sites/all/modules/custom/yield_common/theme/corelation-table.tpl.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Customize the display footer.
+*/
+  drupal_set_title(t("Зведена кореляційна таблиця"));
+?>
+<h1>
+<?php print t("Зведена кореляційна таблиця"); ?>
+</h1>
+<?php print $correlation; ?>
---
+++ sites/all/modules/custom/yield_common/theme/yield-all-crop.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Customize the display footer.
+*/
+  drupal_set_title(t("Прогнозування урожайності"));
+?>
+<h1>
+<?php print t("Прогнозування урожайності"); ?>
+</h1>
+<?php print $select; ?>
+<?php print $all_crop; ?>
